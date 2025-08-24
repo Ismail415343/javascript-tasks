cfda7f7d22f61a6c80eec5923c4d4f33102e2d08
Ismail415343 <muhammadmusa12689@gmail.com>
2025-08-24 14:49:45 +0500

    linear search in the array and the string to find the index of the array and string

---
+++ DAY-13/day-13.js
@@ -0,0 +1,30 @@
+// const linearsearch = (num,target)=>{
+// for(let i = 0; i<num.length;i++){
+
+//     if(target===num[i]){
+//         return i;
+//     }
+// }
+// return -1;
+// }
+// console.log(linearsearch([4,5,6,7,0,1,2],6))
+
+// const alphabhet=(alpha,target)=>{
+//     for(let i =0;i<alpha.target;i++){
+//         if(target === alpha[i]){
+//             return i;
+//         }
+//     }
+// return 0;
+// }
+// console.log(alphabhet(["ISMAIL","HAIDER","ALI","HASSAN"],"ISMAIL"))
+
+const charx =(char,target)=>{
+for(let i=0; i<char.length;i++){
+if(target==char[i]){
+    return i;
+}
+}
+return 0;
+}
+console.log(charx("ismail","l"))
