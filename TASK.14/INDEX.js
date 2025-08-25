9954fb6cefa1f2554d32f920747da110dd4123ef
Ismail415343 <muhammadmusa12689@gmail.com>
2025-08-25 15:37:39 +0500

    // Practice: Find element 10 in [2,4,7,10,15]

--- TASK.14/INDEX.js
+++ TASK.14/INDEX.js
@@ -1,23 +1,17 @@
-// binary search
-const x = (nums,target)=>{
-    let start = 0;
-    let end = nums.length-1;
-
-    while(start <=end){
+// Practice: Find element 10 in [2,4,7,10,15]
+let binarysearch = (num,target)=>{
+    let start =0;
+    let end =num.length-1;
+    while(start<=end){
         let middle = Math.floor((start+end)/2);
-
-        if(nums[middle]===target){
+        if(num[middle]===target){
             return middle;
-        } else if(nums[middle]<target){
-            start = middle + 1;
-        } else{
-            end = middle-1;
+        } else if(num[middle] < target){
+            start = middle +1;
+        } else {
+            end = middle - 1;
         }
     }
-    return -1;
+return -1;
 }
-// time complexity O(logn)
-//space complexity O(1)
-space
-console.log(x([-1,0,3,5,9,12],9))
-console.log(x([-1,0,3,5,9,12],99))
+console.log(binarysearch([2,4,7,10,15],10))
