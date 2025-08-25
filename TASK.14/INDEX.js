d69d154693de0389deb8a642222a7627f055bf6d
Ismail415343 <muhammadmusa12689@gmail.com>
2025-08-25 14:29:08 +0500

    the linear search topic of the task 14

---
+++ TASK.14/INDEX.js
@@ -0,0 +1,25 @@
+// linear search 
+
+// const x = (num,target)=>{
+// for(let i=0;i<num.length;i++){
+//     if(target===num[i]){
+//         return i;
+//     }
+// }
+// return -1;
+// }
+// console.log(x([3,4,2,5,2423,43],43))
+
+
+// const globallinearsearch = (num,target)=>{
+//     const result=[];
+//     for(let i=0; i<num.length;i++){
+// if(target===num[i]){
+//     result.push(i);
+// }
+//     }
+//     if(result.length===0) return -1;
+//     return result
+// }
+
+// console.log(globallinearsearch([2,4,5,6,4,6,0,3,4],4))
