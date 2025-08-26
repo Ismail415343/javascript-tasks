b611f0bb026fda26b7dc8656d27d8c020f56b8ac
Ismail415343 <muhammadmusa12689@gmail.com>
2025-08-26 17:27:06 +0500

    leetcode problems

--- TASK.15/INDEX.js
+++ TASK.15/INDEX.js
@@ -1,12 +1,58 @@
-//  Task: Create one function declaration and one function expression, and test calling them before they’re defined.
+//leetcode Q.1
 
-
-sayhi();
-function sayhi(){
-    console.log("hello world")
+/*for(let i = 0;i<5;i++){
+    console.log(i)
 }
-
-sayhello();
-let sayhello=()=>{
-console.log("hello ismail")
+for(var j = 0; j<6;j++){
+    console.log(j)
 }
+for(const k= 0;k<7;k++){
+    console.log(k)
+}
+console.log(i);
+console.log(k);
+console.log(j);*/
+
+//only var is executed outside the loop because the var is global
+
+
+//Q.2
+
+/*let x = 20;
+function sum (){
+    let y = 30;
+    console.log("the sum of local and global is ",x+y)
+}
+sum()*/
+
+
+//Q.3
+
+/* console.log(a); 
+ var a = 4*/
+
+//It shows undefined, because var moves to the top but the value comes later
+
+/* console.log(b) 
+ let b = 10*/
+
+//It gives an error, because you can’t use let before you write it.
+
+ /*console.log(x) 
+const x = "isail"*/
+
+//It gives an error, because you can’t use const before you write it.
+
+// hello();
+// function hello(){
+// console.log("hello world")
+// 
+
+//It works fine, because normal functions can be used even before you write them.
+
+// sayhi();
+// var sayhi = ()=>{
+//     console.log("helloismail")
+// }
+
+//It gives an error, because here sayhi is not ready yet when you call it.
