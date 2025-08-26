eb2e35da6b6e3408f452c808311bb38708f0cf10
Ismail415343 <muhammadmusa12689@gmail.com>
2025-08-26 15:13:28 +0500

    global scope vs local scope

--- TASK.15/INDEX.js
+++ TASK.15/INDEX.js
@@ -1,19 +1,28 @@
-// scopes
+//global vs local scope
 
-/*let a = 43
-const b =  33
-var c = 53
+// let global = "i am global";
+// function scp(){
+//     let local = "i am local"
+//     console.log(global)
+//     console.log(local)
+// }
+// scp();
 
-console.log(a)
-console.log(b)
-console.log(c)*/
+// console.log(global) // this is accessible because this is define outside the function
+// console.log(local) // this is only execute inside the function
 
-let a = 300; // global scope
+// task 
 
-if(true){
-    let a = 43
-const b =  33
-console.log("inner",a) // block scope
 
-}
-console.log(a)
+
+// let a =12
+// function num(){
+//     let b = 20 
+//     console.log(a)
+//     console.log(b)
+// }
+// num(    )
+
+// console.log(a)
+// console.log(b)
+//the line 27 give error because the b is declared inside the loop which is local scope and the a is executed because it is global
