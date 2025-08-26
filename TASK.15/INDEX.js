0e4d51e2a22af9ef982800b85042a6ce9d93e24d
Ismail415343 <muhammadmusa12689@gmail.com>
2025-08-26 17:10:28 +0500

    Task: Create one function declaration and one function expression, and test calling them before they’re defined.

--- TASK.15/INDEX.js
+++ TASK.15/INDEX.js
@@ -1,56 +1,12 @@
-//hoisting
-
-// function one(){
-//     let username="ismail";
-
-//     function two(){
-//         const app = "vs code"
-//         console.log(username); //using parent child relation
-//         console.log(app)  //
-        
-//     }
-// // console.log(app)
-//  two()
-// }
-// one()
+//  Task: Create one function declaration and one function expression, and test calling them before they’re defined.
 
 
-//let hoisting
+sayhi();
+function sayhi(){
+    console.log("hello world")
+}
 
-// console.log(b);  //ReferenceError 
-// let b = 20; //not executed
-
-// console.log(b); 
-
-// var hoisting
-
-// // console.log(a); //undefined
-// var a = 30;
-// console.log(a); //executed 
-
-//const hoisting
-
-// // console.log(c)
-// const c=40;
-// console.log(40)
-
-
-// function hoisting
-
-
-// hello();
-
-// function hello(){
-//      let x = "hello world";
-//      console.log(x)
-// }
-
-
-// hi()
-
-// let hi = ()=>{
-//     let x =23;
-//     console.log(x)
-// }
-
-//the only normal funstion is executedd before intiallization not the arrow function
+sayhello();
+let sayhello=()=>{
+console.log("hello ismail")
+}
