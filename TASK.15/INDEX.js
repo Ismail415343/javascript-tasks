0bf78e3516cd96f25301a0ecd2704bd5aeafa666
Ismail415343 <muhammadmusa12689@gmail.com>
2025-08-26 16:31:08 +0500

    the let,const and var loops and how the const is executed in for of loop

--- TASK.15/INDEX.js
+++ TASK.15/INDEX.js
@@ -1,25 +1,30 @@
-//-Block Scope (let & const) vs Function Scope (var)
-
-
-
-// if(true){
-//     var x = 70
-// let y = 19
-// const z = 90
+// // Using var
+// for (var i = 0; i < 3; i++) {
+//   console.log("Inside for (var):", i);
 // }
-// console.log(x) only x is accessible
-// console.log(y)
-// console.log(z) 
+// console.log("Outside for (var):", i); 
+// //the var is accessible inside or outside the loop because this is the global 
 
 
-//lets execute all with the use of function
-
-// let x = 20
-// const y = 30
-// function xrp(){
-//     var z = 10
-//     console.log(z) this is only executed inside the function because this is the block scope
+// //Using let
+// for (let j = 0; j < 3; j++) {
+//   console.log("Inside for (let):", j);
 // }
-// xrp();
-// console.log(x)
-// console.log(y)
+// console.log("Outside for (let):", j); 
+// //the let is only executed inside the loop because this is the block/local scope
+
+
+// // Using const
+// for (const k = 0; k < 3; k++) {
+//   console.log("Inside for (const):", k);
+// }
+// // the loop not runs because the constant variable could not change once it declared so the output is only 0
+
+
+//the const loop is only executed in for of loop
+
+const arr = [10,20,30]
+for(const number of arr){
+    console.log(number) //this one is executed
+}
+console.log(number)//this is not gonna execute because this is the local only the number inside the for off loop is executed
