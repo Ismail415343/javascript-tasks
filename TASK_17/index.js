b3e2c973a6bc3da2337f33a0b43492050a41bcb1
Ismail415343 <muhammadmusa12689@gmail.com>
2025-08-29 15:55:48 +0500

    lexical scope

---
+++ TASK_17/index.js
@@ -0,0 +1,22 @@
+
+//lexical scoping
+
+
+/*function outer(){
+
+    let message ="hello world";
+
+    function inner(){
+
+   console.log("message",message)
+
+   function innertwo(){
+
+    console.log("message 2 ", message)
+}
+innertwo();
+    }
+    inner()
+    
+}
+outer();*/
