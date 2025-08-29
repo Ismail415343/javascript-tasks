d963c17173dc1aa79819dd787611a197ecba9c4e
Ismail415343 <muhammadmusa12689@gmail.com>
2025-08-29 16:07:50 +0500

    closures

--- TASK_17/INDEX.js
+++ TASK_17/INDEX.js
@@ -1,22 +1,104 @@
+//closure 
 
-//lexical scoping
 
-
-function outer(){
-
-    let message ="hello world";
-
-    function inner(){
-
-   console.log("message",message)
-
-   function innertwo(){
-
-    console.log("message 2 ", message)
+/*let increment =()=>{
+    let count = 0;
+    count++;
+    console.log(`count increased to ${count}`)
 }
-innertwo();
+
+increment();
+increment();
+increment();
+increment();*/
+
+
+/*function createcounter(){
+    let count = 0;
+
+    function increment(){
+        count++;
+        console.log(`count increased to ${count}`)
     }
-    inner()
-    
+    return {increment}
 }
-outer();
+const counter = createcounter()
+counter.increment()
+counter.increment()
+counter.increment()*/
+
+
+/*let score =0;
+function increasescore(points){
+    score+=points;
+    console.log(`+${points} pts`);
+
+}
+
+
+function decreasescore(points){
+    score-=points;
+    console.log(`-${points} pts`);
+
+}
+function getscore(){
+    return score;
+}
+increasescore(5);
+increasescore(7)
+decreasescore(9);
+decreasescore(5)
+
+console.log(`the final score is ${getscore()} pts`)*/
+
+
+/*let bankAccount=()=> {
+  let balance = 100;
+  return {
+    deposit(amount) {
+      balance += amount;
+      return balance;
+    },
+    withdraw(amount) {
+      balance -= amount;
+      return balance;
+    },
+    getBalance() {
+      return balance;
+    }
+  };
+}
+
+const account = bankAccount();
+console.log(account.deposit(540));  // 150
+console.log(account.getBalance());*/
+
+
+
+
+
+//bank account closure
+
+
+
+
+/*let balance =100;
+function deposit(amount){
+   balance+=amount;
+    console.log(`+${amount} .....`);
+
+}
+
+function withdraw(amount){
+   balance-=amount;
+    console.log(`-${amount} .....`);
+
+}
+function getamount(){
+    return balance;
+}
+deposit(150);
+
+console.log(`the final balance is ${getamount()}....`)*/
+
+
