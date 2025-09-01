ac6dc2180932fcc093d99f13ae71e36c555e0dd3
Ismail415343 <muhammadmusa12689@gmail.com>
2025-09-01 16:48:23 +0500

    TASK 1. Fetch all posts and print first 5 titles. 	2.	Fetch a single post (/posts/1) and log it. 	3.	Try a wrong URL and handle error.

--- TASK_18/INDEX.js
+++ TASK_18/INDEX.js
@@ -1,10 +1,41 @@
-//Using Fetch with Promises
 
-fetch("https://jsonplaceholder.typicode.com/posts")
-.then(response => response.json())
+// 1. fetch all post and print first 5 titles
+
+/*fetch("https://jsonplaceholder.typicode.com/posts")
+.then(response =>response.json())
 .then(data=>{
-console.log("the first 5 posts of ",data.slice(0,4))
+    console.log("the full data of json is",data)
+    console.log("the first 5 posts of json are", data.slice(0,5));
+    
+})
+
+.catch(error=>{
+    console.log("error", error)
+})*/
+
+// 2. fetching single post 
+
+/*fetch("https://jsonplaceholder.typicode.com/posts")
+.then(response=>response.json())
+.then(post=>{
+    console.log("the single post of the json ",post[0])
 })
 .catch(error=>{
-    console.log("error",error)
-});
+    console.log("error occures",error)
+})*/
+
+
+//	3.	Try a wrong URL and handle error.
+
+
+fetch("https://jsonplaceholder.typicddfdode.com/posts")
+.then(response =>response.json())
+.then(data=>{
+    console.log("the full data of json is",data)
+    console.log("the first 5 posts of json are", data.slice(0,5));
+    
+})
+
+.catch(error=>{
+    console.log("error", error)
+})
