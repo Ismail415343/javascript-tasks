4b23c12cd9f1c697f0e9ea02ede3c4c737a47cc6
Ismail415343 <muhammadmusa12689@gmail.com>
2025-09-02 14:15:05 +0500

    the last 2 tasks of task 18

--- TASK_18/INDEX.js
+++ TASK_18/INDEX.js
@@ -1,22 +1,42 @@
-// async function getpost(){
-//     try{
-//         const response = await fetch(https://jsonplaceholder.typicode.com/posts)
-//         const data = await response.json()
-//         console.log("data",data);
-//     } catch(error){
-//         console.log("error",error);
 
-//     }
+// task 
+
+
+//1.	Convert previous fetch examples into async/await.
+
+// async function getposts() {
+//      try {
+//     let response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+//       method: "POST",
+//       body: JSON.stringify({
+//         title: "foo",
+//         body: "bar",
+//         userId: 1
+//       }),
+//       headers: {
+//         "Content-type": "application/json; charset=UTF-8"
+//       }
+//     });
+
+//     let data = await response.json();
+//     console.log(data);
+
+//   } catch (error) {
+//     console.log("Error:", error);
+//   }
 // }
 
-//the first 5 of the array
 
-/*async function getpost() {
+//	2.	Create a function getUserPosts(userId) that fetches posts of a specific user.
+
+async function getuser(userid){
     try{
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts")
-        const data = await response.json()
-        console.log("the first five slides of the json", data.slice(0,5))
+        let response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userid}`)
+        let data = await response.json();
+        console.log("data",data)
     }catch(error){
-        console.log("error: ",error)
+        console.log("error",error)
     }
-}*/
+}
+
+getuser(5)
