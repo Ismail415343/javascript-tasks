483f018fa7af9957ee45111936919a650378b843
Ismail415343 <muhammadmusa12689@gmail.com>
2025-09-02 13:46:30 +0500

    3. POST Request with Fetch

--- TASK_18/INDEX.js
+++ TASK_18/INDEX.js
@@ -1,41 +1,32 @@
 
-// 1. fetch all post and print first 5 titles
 
-/*fetch("https://jsonplaceholder.typicode.com/posts")
-.then(response =>response.json())
-.then(data=>{
-    console.log("the full data of json is",data)
-    console.log("the first 5 posts of json are", data.slice(0,5));
-    
+//POST Request with Fetch
+
+// fetch("https://jsonplaceholder.typicode.com/posts", {
+//   method: "POST",
+//   body: JSON.stringify({
+//     title: "foo",
+//     body: "bar",
+//     userId: 1
+//   }),
+//   headers: {
+//     "Content-type": "application/json; charset=UTF-8"
+//   }
+// })
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+
+fetch("https://jsonplaceholder.typicode.com/posts",{
+    method: "POST",
+    body:JSON.stringify({
+        title : "hello",
+        body:"bar",
+        userid:4
+    }),
+    headers: {
+        "content-type": "application/json; charset=utf-8"
+    }
 })
-
-.catch(error=>{
-    console.log("error", error)
-})*/
-
-// 2. fetching single post 
-
-/*fetch("https://jsonplaceholder.typicode.com/posts")
 .then(response=>response.json())
-.then(post=>{
-    console.log("the single post of the json ",post[0])
-})
-.catch(error=>{
-    console.log("error occures",error)
-})*/
-
-
-//	3.	Try a wrong URL and handle error.
-
-
-fetch("https://jsonplaceholder.typicddfdode.com/posts")
-.then(response =>response.json())
-.then(data=>{
-    console.log("the full data of json is",data)
-    console.log("the first 5 posts of json are", data.slice(0,5));
-    
-})
-
-.catch(error=>{
-    console.log("error", error)
-})
+.then(data=> console.log(data))
