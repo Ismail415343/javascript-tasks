06a9db9bfe8855c9737ebe36a51ddf191fe495b8
Ismail415343 <muhammadmusa12689@gmail.com>
2025-09-02 13:56:39 +0500

    4. Async/Await with Fetch

--- TASK_18/INDEX.js
+++ TASK_18/INDEX.js
@@ -1,32 +1,22 @@
+// async function getpost(){
+//     try{
+//         const response = await fetch(https://jsonplaceholder.typicode.com/posts)
+//         const data = await response.json()
+//         console.log("data",data);
+//     } catch(error){
+//         console.log("error",error);
 
+//     }
+// }
 
-//POST Request with Fetch
+//the first 5 of the array
 
-// fetch("https://jsonplaceholder.typicode.com/posts", {
-//   method: "POST",
-//   body: JSON.stringify({
-//     title: "foo",
-//     body: "bar",
-//     userId: 1
-//   }),
-//   headers: {
-//     "Content-type": "application/json; charset=UTF-8"
-//   }
-// })
-//   .then(response => response.json())
-//   .then(data => console.log(data));
-
-
-fetch("https://jsonplaceholder.typicode.com/posts",{
-    method: "POST",
-    body:JSON.stringify({
-        title : "hello",
-        body:"bar",
-        userid:4
-    }),
-    headers: {
-        "content-type": "application/json; charset=utf-8"
+/*async function getpost() {
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+        const data = await response.json()
+        console.log("the first five slides of the json", data.slice(0,5))
+    }catch(error){
+        console.log("error: ",error)
     }
-})
-.then(response=>response.json())
-.then(data=> console.log(data))
+}*/
