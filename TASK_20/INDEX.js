9ec51837a14efc041e729b67beede0b1d59e9bd2
Ismail415343 <muhammadmusa12689@gmail.com>
2025-09-03 14:15:36 +0500

    class decleration and constructor method

---
+++ TASK_20/INDEX.js
@@ -0,0 +1,48 @@
+// Class Declaration
+
+class car{
+    constructor(brand,model){
+        this.brand=brand;
+        this.model=model;
+    }
+getdetails(){
+    console.log(`${this.brand} ${this.model}`)  ;
+}
+
+}
+let c1 = new car("toyota",2009)
+let c2 = new car ("toyota",1998)
+c1.getdetails()
+c2.getdetails()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//class simple
+
+// class Product{
+//     constructor(name,price){
+//         this.name = name;
+//         this.price = price;
+//     }
+//     displayproduct(){
+// console.log(`the price of the product is ${this.price}$`)
+// console.log(`the name of the product is ${this.name}`)
+//     }
+// }
+// let p1 = new Product(23,"pen");
+// let p2 = new Product(30,"chips")
+
+// p1.displayproduct()
