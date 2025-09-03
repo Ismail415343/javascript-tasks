a9db11bc3bb98288f6482990737b2124ab8c2265
Ismail415343 <muhammadmusa12689@gmail.com>
2025-09-03 14:26:28 +0500

    creating multiple instance

--- TASK_20/INDEX.js
+++ TASK_20/INDEX.js
@@ -1,18 +1,20 @@
 // creating multiple instances4
 
-// class car {
-//     constructor(brand,model){
-//         this.brand = brand
-//         this.model=model
-//     }
+/*class car {
+    constructor(brand,model){
+        this.brand = brand
+        this.model=model
+    }
 
-// }
-// const c1 = new car("bmw ","m5")
-// const c2 = new car("honda ","rs")
-// console.log(c1.brand + c1.model)
-// console.log(c2.brand + c2.model)
+}
+const c1 = new car("bmw ","m5")
+const c2 = new car("honda ","rs")
+console.log(c1.brand + c1.model)
+console.log(c2.brand + c2.model)*/
 
-class product {
+
+
+/*class product {
     constructor(price,name,quantity){
         this.price=price;
         this.quantity=quantity
@@ -22,4 +24,4 @@ class product {
 }
 
 let p1 = new product("10$ ","pen ",1000);
-console.log(p1.name+ p1.quantity)
+console.log(p1.name+ p1.quantity)*/
