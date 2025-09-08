5bb4f9090c04c38c4d775c30e5f6c57012303893
Ismail415343 <muhammadmusa12689@gmail.com>
2025-09-08 14:31:10 +0500

    //inheritance & super()

---
+++ TASK_21/INDEX.js
@@ -0,0 +1,27 @@
+//inheritance & super()
+
+/*class animal{
+    constructor(name){
+this.name= name;
+    }
+     speak(){
+return `${this.name} make a sound`
+    }
+}
+class dog extends animal{
+    constructor(name,breed){
+        super(name);
+        this.breed=breed
+    }
+    speak(){
+        return `${this.name} dog breed is ${this.breed}, and all dogs bark`
+    }
+}
+
+const a1 = new animal("all animals");
+console.log(a1.speak())
+
+const d1 = new dog("boozo","husky")
+console.log(d1.speak())*/
+
+
