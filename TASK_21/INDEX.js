456b2f59acf2130c8da4b4bd05e5c5a4cf1ea951
Ismail415343 <muhammadmusa12689@gmail.com>
2025-09-09 15:18:41 +0500

    leetcode problem

--- TASK_21/INDEX.js
+++ TASK_21/INDEX.js
@@ -1,47 +1,21 @@
-//– Private Fields & Practical Task
+function isValid(s) {
+    const stack = [];
+    const mapping = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
 
-/*class bankacc{
-    #balance=90;
-    deposit(amount){
-this.#balance +=amount; 
-    }
-get balance (){
-    return this.#balance;
+for(let char of s){
+if(char==="(" ||char==="[" ||char==="{" ){
+    stack.push(char)
+} else {
+if(stack.length===0||stack.pop()!==mapping[char]){
+return false
 }
 }
-let acc = new bankacc()
-acc.deposit(190);
-console.log(acc.balance)*/
+}
+return stack.length===0
 
-class bankacc{
-    #balance;
-    constructor(initialbalance = 0){
-this.#balance=initialbalance;
-    }
-
-deposit(amount){
-if(amount>0){
-    this.#balance+=amount
-    console.log(`deposit: $${amount}`)
-} else{
-    console.log("deposit amount must be positive")
 }
-}
-withdrawl(amount){
-if(amount > 0 && amount <= this.balance){
-    this.#balance  -= amount
-    console.log(`withdrawl $${amount}`)
-} else{
-    console.log("Insufficient balance or invalid amount!");
-}
-}
-
-get balance(){
-   return this.#balance
-}
-}
-let acc = new bankacc();
-acc.deposit(80);
-console.log("balance after deposit is ",acc.balance);
-acc.withdrawl(56)
-console.log("balance after withdrawl is ",acc.balance);
+console.log(isValid("{[()]}"))
