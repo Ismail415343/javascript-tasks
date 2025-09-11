097c6cc05c07415951c0aa0f691f0d2adfe60fdb
Ismail415343 <muhammadmusa12689@gmail.com>
2025-09-11 16:24:28 +0500

    practice task

--- TASK_22/INDEX.js
+++ TASK_22/INDEX.js
@@ -1,68 +1,20 @@
-//2. Inheritance with Prototype
+function Shape() {}
 
-// function ANIMAL(name){
-// this.name=name;
-// }
-// ANIMAL.prototype.speak=function(){
-//     console.log(this.name +" makes a sound");
-// }
+Shape.prototype.area = function() {
+  return 0;
+};
 
-// function dog(name){
-//     ANIMAL.call(this,name)
-// }
-// dog.prototype = Object.create(ANIMAL.prototype)
-// dog.prototype.constructor = dog;
-
-// dog.prototype.speak = function(){
-// console.log(this.name," barks");
-// }
-
-
-// let d1 = new ANIMAL("buddy")
-// d1.speak()
-
-
-// function person(name,age){
-//     this.name=name;
-//     this.age=age;
-// }
-// person.prototype.method=function(){
-//     console.log(this.name + "is "+ this.age +" years old");   
-// }
-// function employee(name,age,salary){
-//     person.call(this,name,age);
-//     this.salary=salary;
-// }
-// employee.prototype = Object.create(person.prototype)
-// employee.prototype.constructor=employee;
-
-// employee.prototype.method=function (){
-// console.log(`the name of the person is${this.name}, he is ${this.age} years old and his salary is ${this.salary}$`);
-
-// }
-// let p1 = new employee("ismail",19, 1000)
-// p1.method()
-
-
-// 3.Relation to ES6 Classes
-
-/*class animal{
-    constructor(name){
-        this.name=name
-    }
-speak(){
-    console.log(this.name,"makes a sound");
-    
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
 }
-}
-class dog extends animal{
-   speak(){
-    console.log(this.name,"barks");
-    
-   }
 
-}
-let dp = new animal("boozo")
-let op = new dog("boozo")
-dp.speak()
-op.speak()*/
+Rectangle.prototype = Object.create(Shape.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
+Rectangle.prototype.area = function() {
+  return this.width * this.height;
+};
+
+let r1 = new Rectangle(5, 8);
+console.log(r1.area()); 
