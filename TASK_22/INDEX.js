79baf920c66608ef4b8658104df21fdea4da99d9
Ismail415343 <muhammadmusa12689@gmail.com>
2025-09-15 15:11:50 +0500

    inheritance & super()

--- TASK_22/INDEX.js
+++ TASK_22/INDEX.js
@@ -1,55 +1,26 @@
-//leet code problem
+//inheritance & super()
 
-/*let reverseNum = (num)=>{
-let myreversenum = num.toString().split("").reverse().join("")
-if(myreversenum.endsWith("-")){
-myreversenum = "-" + myreversenum
-return parseInt(myreversenum)
-} else {
-  return parseInt(myreversenum)
+/*class animal{
+    constructor(name){
+this.name= name;
+    }
+     speak(){
+return `${this.name} make a sound`
+    }
+}
+class dog extends animal{
+    constructor(name,breed){
+        super(name);
+        this.breed=breed
+    }
+    speak(){
+        return `${this.name} dog breed is ${this.breed}, and all dogs bark`
+    }
 }
 
-}
-console.log(reverseNum(-1234))*/
+const a1 = new animal("all animals");
+console.log(a1.speak())
 
-//Complexity:
+const d1 = new dog("boozo","husky")
+console.log(d1.speak())*/
 
-//Time Complexity: O(n) 
-//where n is the number of digits in x.
-//(We convert to string, reverse, and parse — each step linear in digits.)
-
-//Space Complexity: O(n) 
-//extra space is needed for the string and array during reversal.
-
-
-//problem.2
-
-/*let removeDuplicate = (num)=>{
-if(num.length === 0 ){
-return 0;
-}
-let i = 0;
-
-for(let j=1;j<num.length;j++){
-  if(num[j] !==num[i]  ){
-    i++;
-    num[i]=num[j]
-  }
-}
-return i +1;
-}
-
-let array=[1,2,2,3]
-let lgth = removeDuplicate(array);
-console.log(lgth)
-console.log(array.slice(0,lgth));*/
-
-
-
-//Complexity:
-
-//Time Complexity: O(n) 
-//one pass through the array with two pointers.
-
-//Space Complexity: O(1) 
-//in-place, no extra array is used.
