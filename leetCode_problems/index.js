8e1cdf760a4088d7d596ecf3fe8d41c19ec76e66
Ismail415343 <muhammadmusa12689@gmail.com>
2026-02-03 16:30:50 +0500

    .

--- leetCode_problems/index.js
+++ leetCode_problems/index.js
@@ -12,3 +12,34 @@ let fn =(arr)=>{
 }
 const word = ["apple", ""]
 console.log(fn(word))
+
+
+// 3. Longest Substring Without Repeating Characters
+
+let longestCharacterLength =(s)=>{
+
+
+    let maxLength = 0;
+
+    for(let i = 0; i < s.length; i++){
+        let visited = ''
+
+        for(let j = i; j< s.length; j++){
+            if (visited.includes(s[j])) {
+                break;
+            }
+              visited += s[j];
+
+            if (visited.length > maxLength) {
+                maxLength = visited.length;
+            }
+        }
+
+
+    }
+    return maxLength;
+}
+
+console.log(longestCharacterLength("abcabcbb")); 
+console.log(longestCharacterLength("bbbbb"));   
+console.log(longestCharacterLength("pwwkew")); 
